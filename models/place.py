1776fedbe240372faf1815e28db992b62d688032
magezil <256@holbertonschool.com>
2018-03-27 04:41:57 +0000

    Complete getter and setters

--- models/place.py
+++ models/place.py
@@ -25,24 +25,45 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     user = relationship("User", back_populates="places")
     place_amenity = Table('association', Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
-            )
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'), nullable=False))
+    amenity_ids = []
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", "place")
-        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenities")
+        amenities =\
+            relationship("Amenity", secondary=place_amenity,
+                         viewonly=False, back_populates="place_amenities")
     else:
+        reviews = self.reviews
+        amenities = self.amenities
+
+    @property
+    def reviews(self):
+        """
+            Property reviews: reviews associated with place.id
+        """
         reviews = storage.all(Review)
         my_reviews = []
         for review in reviews:
             if review.place_id == self.id:
                 my_reviews.append(review)
-
-"""
-Getter attribute amenities that returns the list of Amenity instances based on the attribute amenity_ids that contains all Amenity.id linked to the Place
-Setter attribute amenities that handles append method for adding an Amenity.id to the attribute amenity_ids. This method should accept only Amenity object, otherwise, do nothing.
-"""
-    amenity_ids = []
-
         return my_reviews
+
+    @property
+    def amenities(self):
+        """
+            Property amenities: amenities associated with place.id
+
+            Setter validates obj is Amenity
+
+            Parameter:
+                obj: object to append obj.id to amenity_ids
+        """
+        return Place.amenity_ids
+
+    def amenities(self, obj):
+        if type(obj) is Amenity:
+            Place.amenity_ids.append(obj.id)
