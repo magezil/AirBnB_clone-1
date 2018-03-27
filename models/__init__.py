e4065d95f734d1a64fe3971603dab7d800829075
magezil <256@holbertonschool.com>
2018-03-27 20:19:04 +0000

    Fix logic of using dbor not

--- models/__init__.py
+++ models/__init__.py
@@ -18,10 +18,10 @@ classes = {"User": User, "BaseModel": BaseModel,
            "Review": Review}
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-else:
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
 storage.reload()
