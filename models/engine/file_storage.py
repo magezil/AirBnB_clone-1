c3291f8f063a213c1145bcc77fbe1af75b929708
magezil <256@holbertonschool.com>
2018-03-28 19:34:16 +0000

    Fix all <class>

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,7 +22,7 @@ class FileStorage:
         if cls is None:
             return self.__objects
         for key, val in FileStorage.__objects.items():
-            if cls == val.__class__:
+            if cls == val.__class__.__name__:
                 objs[key] = val
         return objs
 
