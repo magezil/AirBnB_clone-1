e8d03bf8090245260d5d4af21e88766998c48eed
magezil <256@holbertonschool.com>
2018-03-29 05:02:41 +0000

    Defaults for id, created, updated if not in kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -33,14 +33,26 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
+            return
+
+        if "created_at" in kwargs:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
+                                                 "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.created_at = datetime.now()
+
+        if "updated_at" in kwargs:
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+                                                 "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.updated_at = datetime.now()
+
+        if "id" not in kwargs:
+            self.id = str(uuid.uuid4())
+
+        for key, val in kwargs.items():
+            if "__class__" not in key:
+                setattr(self, key, val)
 
     def __str__(self):
         '''
