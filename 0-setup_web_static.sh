7a4677073cf40d287363dbfdf3c990d360136a49
magezil <256@holbertonschool.com>
2018-04-02 22:42:55 +0000

    Add suod

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # Prepare web servers for deployment
-apt-get -y update
-apt-get -y install nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 
 new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
-sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
+sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
 
-service nginx restart
+sudo service nginx restart
