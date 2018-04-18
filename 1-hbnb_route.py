3178f637ad7adac1fd375ce740b5f66324f048e5
magezil <256@holbertonschool.com>
2018-04-18 17:07:48 +0000

    Start using Flask

---
+++ 1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Start Flask web application that listens on 0.0.0.0:5000
+    Has route / that displays "Hello HBNB!"
+    Has route /hbnb that displays "HBNB"
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+        Route / displays "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world():
+    """
+        Route /hbnb displays "Hello HBNB!"
+    """
+    return 'Hello HBNB!'
