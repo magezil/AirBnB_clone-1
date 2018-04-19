e4e7a0d54b35c8cea0c60a13c859bdfb14497915
magezil <256@holbertonschool.com>
2018-04-19 04:38:20 +0000

    Send list instead of dictionary to template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,7 +14,7 @@ def states_list():
         Route /states_list displays HTML page with States listed alphabetically
     """
     states = storage.all(classes["State"])
-    return render_template('7-states_list.html', states=states)
+    return render_template('7-states_list.html', states=states.values())
 
 
 @app.teardown_appcontext
