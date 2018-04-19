dcfb53917cbd3224117507a5a524bceaa456c2bc
magezil <256@holbertonschool.com>
2018-04-19 21:10:04 +0000

    Add Route states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,6 +9,7 @@ from models import storage, classes
 app = Flask(__name__)
 
 
+@app.route('/states', strict_slashes=False)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
